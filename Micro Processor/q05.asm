ce3581bc27e64c324d91259ba36a0eb362f8d1d0
MaxySpark <maxyspark@gmail.com>
2016-12-30 02:22:07 +0530

    q5 indent

--- Micro Processor/q05.asm
+++ Micro Processor/q05.asm
@@ -2,10 +2,10 @@ LXI H,6005H
 LXI B,6008H
 MVI D,06H
 LOOP:	MOV A,M
-	STAX B
-	MVI M,00H
-	DCX H
-	DCX B
-	DCR D
-	JNZ LOOP
-	HLT
+        STAX B
+        MVI M,00H
+        DCX H
+        DCX B
+        DCR D
+        JNZ LOOP
+        HLT
